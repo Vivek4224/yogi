8fc713b588acfc7add771b69de36ebf368576adc
Vivek4224 <vivekbarvaliya4224@gmail.com>
2025-01-17 13:22:21 +0530

    update site

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Vivek" };
+  const loggedIn = {
+    firstName: "Vivek",
+    lastName: "Barvaliya",
+    email: "vivekbarvaliya@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.25 }]}
+      />
     </section>
   );
 };
